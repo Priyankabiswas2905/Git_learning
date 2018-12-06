5c3201f34464ee1c7d9c01e58abfd3a5287b67a6
toddn <todd.nicholson@gmail.com>
2018-12-05 18:52:43 -0600

    still need to add method for json for each object

--- app/services/TreeService.scala
+++ app/services/TreeService.scala
@@ -20,8 +20,13 @@ trait TreeService {
 
   def getChildrenOfSpace(space : Option[ProjectSpace], mine : Boolean, user : User) :List[JsValue]
 
+  def getSpaces(mine : Boolean, user: User) : List[JsValue]
+
   def getChildrenOfCollection(collection: Option[Collection], mine : Boolean, user : User) :List[JsValue]
+
+  def getCollections(mine: Boolean, user : User) : List[JsValue]
 
   def getChildrenOfDataset(dataset: Option[Dataset], mine : Boolean, user : User) :List[JsValue]
 
+  def getDatasets(mine: Boolean, user: User) : List[JsValue]
 }
--- app/services/mongodb/MongoDBTreeService.scala
+++ app/services/mongodb/MongoDBTreeService.scala
@@ -1,5 +1,7 @@
 package services.mongodb
 
+import api.Permission
+import api.Permission.Permission
 import javax.inject.{Inject, Singleton}
 import models._
 import play.api.libs.json.JsValue
@@ -27,7 +29,7 @@ class MongoDBTreeService @Inject() (
             case None =>
           }
         }
-        case None =>
+        case None => children = getSpaces(mine, user)
       }
     } else if (nodeType == "collection") {
       nodeId match {
@@ -39,7 +41,7 @@ class MongoDBTreeService @Inject() (
             case None =>
           }
         }
-        case None =>
+        case None => children = getCollections(mine, user)
       }
     } else if (nodeType == "dataset") {
       nodeId match {
@@ -51,7 +53,7 @@ class MongoDBTreeService @Inject() (
             case None =>
           }
         }
-        case None =>
+        case None => children = getDatasets(mine, user)
       }
     }
     children
@@ -75,6 +77,33 @@ class MongoDBTreeService @Inject() (
   def getChildrenOfDataset(dataset: Option[Dataset], mine: Boolean, user : User): List[JsValue] = {
     var children : ListBuffer[JsValue] = ListBuffer.empty[JsValue]
     children.toList
+  }
+
+  def getSpaces(mine : Boolean, user: User) : List[JsValue] = {
+    var rootSpaces : ListBuffer[JsValue] = ListBuffer.empty[JsValue]
+    var spaces = spaceService.listAccess(0,Set[Permission](Permission.ViewSpace),Some(user),true,true,false,false)
+    if (mine){
+      spaces = spaces.filter((s: ProjectSpace) => (s.creator == user.id))
+    }
+    rootSpaces.toList
+  }
+
+  def getDatasets(mine : Boolean, user: User) : List[JsValue] = {
+    var visibleDatasets : ListBuffer[JsValue] = ListBuffer.empty[JsValue]
+    var ds = datasets.listAccess(0,Set[Permission](Permission.ViewDataset),Some(user),true,true,false)
+    if (mine){
+      ds = ds.filter((d: Dataset) => (d.author == user))
+    }
+    visibleDatasets.toList
+  }
+
+  def getCollections(mine: Boolean, user: User) : List[JsValue] = {
+    var cols = collections.listAccess(0,Set[Permission](Permission.ViewCollection),Some(user),true,true,false)
+    if (mine) {
+      cols = cols.filter((c: Collection) => (c.author == user))
+    }
+    var visibleCollection : ListBuffer[JsValue] = ListBuffer.empty[JsValue]
+    visibleCollection.toList
   }
 
 }
