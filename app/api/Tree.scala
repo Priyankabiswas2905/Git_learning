a734ae65ddd2b5aded0389a5bbecf8431da9eefb
toddn <todd.nicholson@gmail.com>
2018-12-07 10:35:51 -0600

    new api class, will use service methods

---
+++ app/api/Tree.scala
@@ -0,0 +1,18 @@
+package api
+
+import javax.inject.Inject
+import api.Permission.Permission
+import models._
+import play.api.libs.json._
+import play.api.libs.json.Json.toJson
+import services._
+import scala.collection.immutable.List
+import scala.collection.mutable.ListBuffer
+
+class Tree @Inject()(
+  treeService: TreeService) extends ApiController {
+
+  
+
+
+}
