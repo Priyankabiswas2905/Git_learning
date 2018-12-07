bbde9d5af5a251d6f525a25392cd58dbb960a223
toddn <todd.nicholson@gmail.com>
2018-12-07 10:44:00 -0600

    removed unnecessary imports

--- app/api/Tree.scala
+++ app/api/Tree.scala
@@ -1,13 +1,8 @@
 package api
 
 import javax.inject.Inject
-import api.Permission.Permission
-import models._
-import play.api.libs.json._
 import play.api.libs.json.Json.toJson
 import services._
-import scala.collection.immutable.List
-import scala.collection.mutable.ListBuffer
 
 class Tree @Inject()(
   treeService: TreeService) extends ApiController {
@@ -21,6 +16,5 @@ class Tree @Inject()(
       case None => BadRequest("No user supplied")
     }
   }
-
 
 }
