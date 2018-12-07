d33c9b33180d69971c4884cc19a6c8aa6ec28646
toddn <todd.nicholson@gmail.com>
2018-12-07 10:38:25 -0600

    api for tree added

--- app/api/Tree.scala
+++ app/api/Tree.scala
@@ -12,7 +12,15 @@ import scala.collection.mutable.ListBuffer
 class Tree @Inject()(
   treeService: TreeService) extends ApiController {
 
-
+  def getChildrenOfNode(nodeType: String, nodeId: Option[String], mine: Boolean) = PrivateServerAction { implicit request =>
+    request.user match {
+      case Some(user) => {
+        var result = treeService.getChildrenOfNode(nodeType,nodeId,mine,user)
+        Ok(toJson(result))
+      }
+      case None => BadRequest("No user supplied")
+    }
+  }
 
 
 }
