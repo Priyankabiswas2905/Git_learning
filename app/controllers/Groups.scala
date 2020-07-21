37022b4e4042ced8c92476f33613dbb010171701
Ayush Sarkar <ayushs2@illinois.edu>
2020-07-20 19:18:56 -0700

    Fixed a lot of the syntax errors. Code compiles. Listing groups page looks good so far, but have to work on group creation.

--- app/controllers/Groups.scala
+++ app/controllers/Groups.scala
@@ -1,10 +1,31 @@
 package controllers
 
+import java.net.URL
+import java.util.{ Calendar, Date }
 import javax.inject.Inject
+
+import api.Permission
+import api.Permission._
 import models._
+import play.api.{ Logger, Play }
+import play.api.data.Forms._
+import play.api.data.{ Form, Forms }
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.i18n.Messages
 import services._
+import securesocial.core.providers.{ Token, UsernamePasswordProvider }
+import org.joda.time.DateTime
+import play.api.i18n.Messages
+import play.api.libs.ws._
+import services.AppConfiguration
+import util.{ Formatters, Mail, Publications }
 
 import scala.collection.immutable.List
+import scala.collection.mutable.{ ArrayBuffer, ListBuffer }
+import scala.concurrent.{ Future, Await }
+import scala.concurrent.duration._
+import org.apache.commons.lang.StringEscapeUtils.escapeJava
 
 
 case class groupFormData(
@@ -46,86 +67,70 @@ class Groups @Inject()(
       "role" -> nonEmptyText,
       "message" -> optional(text))((addresses, role, message) => groupInviteData(addresses = addresses, role = role, message = message))((d: groupInviteData) => Some(d.addresses, d.role, d.message)))
 */
-
-
-
-//  def list2(numPage : Int, limit: Int, mode:String, owner: Option[String]) = UserAction(needActive = false) { implicit request =>
-//    implicit val user = request.user
-//    request.user match {
-//      case Some(user) => {
-//
-//      }
-//      case None=> {
-//
-//      }
-//    }
-//    val nextExist = false
-//    val prevExist = false
-//    val person = owner.flatMap(o => users.get(UUID(o)))
-//    val ownerName = person match{
-//      case Some(p) => Some(p.fullName)
-//      case None => None
-//    }
-//    var title : Option[String] = Some(Messages("list.title", Messages("groups.title")))
-//    val tempList = groups.listGroupsByUser(UUID(user))
-//    val rem = tempList.length % limit
-//    val div = tempList/limit
-//    val maxPage = {
-//      if(rem == 0)
-//        maxPage = div
-//      else
-//        maxPage = div + 1
-//    }
-//    if(numPage < 1){
-//      nextExist = false
-//      prevExist = false
-//    }
-//    if(numPage < maxPage)
-//      nextExist = true
-//    if(numPage > 1)
-//      prevExist = true
-//    val groupList = person match {
-//      case(Some(p)) => {
-//        title = Some(Messages("owner.title", p.fullName, Messages("groups.title")))
-//        if((numPage < maxPage) || (numPage == maxPage && rem == 0)){
-//          val startIndex = limit * (numPage - 1)
-//          val endIndex = startIndex + (limit - 1)
-//          tempList.slice(startIndex, endIndex + 1)
-//        }
-//        else if(numPage == maxPage && rem != 0){
-//          tempList.takeRight(rem)
-//        }
-//      }
-//      case None => {
-//        InternalServerError("No user supplied")
-//      }
-//    }
-//
-//    //Code to read the cookie data. On default calls, without a specific value for the mode, the cookie value is used.
-//    //Note that this cookie will, in the long run, pertain to all the major high-level views that have the similar
-//    //modal behavior for viewing data. Currently the options are tile and list views. MMF - 12/14
-//    val viewMode: Option[String] =
-//    if (mode == null || mode == "") {
-//      request.cookies.get("view-mode") match {
-//        case Some(cookie) => Some(cookie.value)
-//        case None => None //If there is no cookie, and a mode was not passed in, the view will choose its default
-//      }
-//    } else {
-//      Some(mode)
-//    }
-//
-//    Ok(views.html.groups.listgroups(groupList,limit, owner, ownerName, viewMode, prevExist, nextExist, title))
-//  }
-
-  def list() = UserAction(needActive = false) { implicit request =>
-    request.user match {
-      case Some(user) => {
-        val groupList : List[Group] = groups.listGroupsByUser(user.id)
-        Ok(views.html.blank())
-        // Ok(views.html.groups.listgroups(groupList,10, Some(user.id.toString), Some(user.fullName), Some("tile"), false, false, Some("title")))
-      }
-      case None=> BadRequest("No user")
+  def list(numPage : Int, limit: Int, mode:String, owner: Option[String], nextPage : Int, prevPage :Int) = UserAction(needActive = false) { implicit request =>
+    implicit val requestUser = request.user
+    var nextExist = false
+    var prevExist = false
+    val person = owner.flatMap(o => users.get(UUID(o)))
+    val ownerName = person match{
+      case Some(p) => Some(p.fullName)
+      case None => None
     }
+    var title : Option[String] = Some(Messages("list.title", Messages("groups.title")))
+    val tempList : List[models.Group] = groups.listGroupsByUser(requestUser.get.id)
+    val rem = tempList.length % limit
+    val div = tempList.length/limit
+    val maxPage = {
+      if(rem == 0)
+        div
+      else
+        div + 1
+    }
+    val next = numPage + 1
+    val prev = numPage - 1
+    if(numPage < 1){
+      nextExist = false
+      prevExist = false
+    }
+    if(numPage < maxPage)
+      nextExist = true
+    if(numPage > 1)
+      prevExist = true
+    val groupList : List[models.Group] = person match {
+      case(Some(p)) => {
+        title = Some(Messages("owner.title", p.fullName, Messages("groups.title")))
+        if((numPage < maxPage) || (numPage == maxPage && rem == 0)){
+          val startIndex = limit * (numPage - 1)
+          val endIndex = startIndex + (limit - 1)
+          tempList.slice(startIndex, endIndex + 1)
+        }
+        else if(numPage == maxPage && rem != 0){
+          tempList.takeRight(rem)
+        }
+        else
+          tempList
+      }
+      case None => {
+        InternalServerError("No user supplied")
+        List.empty
+      }
+    }
+
+    //val decodedGroupList = groupList.map(Utils.decodeGroupElements)
+    //Code to read the cookie data. On default calls, without a specific value for the mode, the cookie value is used.
+    //Note that this cookie will, in the long run, pertain to all the major high-level views that have the similar
+    //modal behavior for viewing data. Currently the options are tile and list views. MMF - 12/14
+    val viewMode: Option[String] =
+    if (mode == null || mode == "") {
+      request.cookies.get("view-mode") match {
+        case Some(cookie) => Some(cookie.value)
+        case None => None //If there is no cookie, and a mode was not passed in, the view will choose its default
+      }
+    } else {
+      Some(mode)
+    }
+
+    Ok(views.html.groups.listgroups(groupList, numPage, limit, owner, ownerName, viewMode, prevExist, nextExist, title, next, prev))
   }
 
 
@@ -140,8 +145,8 @@ class Groups @Inject()(
 
   def getGroup(id:UUID) = AuthenticatedAction{implicit request =>
     implicit val user = request.user
-    Ok(views.html.blank())
-  }
+    Ok(views.html.groups.blank())
+    }
 
 
 
@@ -151,9 +156,8 @@ class Groups @Inject()(
     user match {
       case Some(u) => {
         val appConfig: AppConfigurationService = DI.injector.getInstance(classOf[AppConfigurationService])
-        val groupList : List[Group] = groups.listByCreator(u.id)
-        Ok(views.html.blank())
-        // Ok(views.html.groups.listgroups(groupList, 10,Some(u.id.toString),Some(""),Some("tile"),false,false,Some("title") ))
+        val groupList = groups.listByCreator(u.id)
+        Ok(views.html.groups.blank())
 
       }
       case None => {
@@ -161,4 +165,5 @@ class Groups @Inject()(
       }
     }
   }
+
 }
